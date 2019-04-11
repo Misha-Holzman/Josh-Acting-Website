00f98bae59e9bb2c0ed2382439f95c377a097eaa
Misha Holzman <mcholzman1@gmail.com>
2019-04-11 15:43:38 -0400

    media page done and responsive

--- src/Media/index.js
+++ src/Media/index.js
@@ -10,8 +10,11 @@ class Media extends Component {
           <img className='media-tab-imgs-of-josh' src='https://i.imgur.com/695n38c.png' />
           <img className='media-tab-imgs-of-josh' src='https://i.imgur.com/MP5HeMD.png' />
           <img className='media-tab-imgs-of-josh-small' src='https://i.imgur.com/q22SvcL.jpg' />
-          <img className='media-tab-imgs-of-josh' src='https://i.imgur.com/CuUDmBc.png' />
           <img className='media-tab-imgs-of-josh-small' src='https://i.imgur.com/pJbEz7B.png' />
+          <img className='media-tab-imgs-of-josh-small' src='https://i.imgur.com/45LXsua.png' />
+          <img className='media-tab-imgs-of-josh-small' src='https://i.imgur.com/8hKI2e0.png' />
+          <img className='media-tab-imgs-of-josh-small' src='https://i.imgur.com/4JOJysG.png' />
+          <img className='media-tab-imgs-of-josh' src='https://i.imgur.com/CuUDmBc.png' />
         </div>
         {/* <div className='Media-credit-div'>
           <h6 className='Media-giving-credit-text'>Media by <a href='http://www.davidnoles.com/' target='_blank' className='link-to-headshot-guy'>David Noles Photography</a></ h6>
