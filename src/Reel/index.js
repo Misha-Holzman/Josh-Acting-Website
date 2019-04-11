36dab30dec37695a6cdca69612284a7ebbc9ea55
Misha Holzman <mcholzman1@gmail.com>
2019-04-11 17:01:40 -0400

    reel page done and responsive

--- src/Reel/index.js
+++ src/Reel/index.js
@@ -2,14 +2,13 @@ import React, { Component } from 'react'
 import ReactPlayer from 'react-player'
 import './style.css'
 
-
 class Reel extends Component {
   render () {
     return (
       <div className='reel-main-container'>
         <div className='reel-content-container-div'>
           <h3 className='reel-title' id='real-jump-link'>Reel</h3>
-          <ReactPlayer url='https://vimeo.com/318350985' style={{  }} />
+          <ReactPlayer url='https://vimeo.com/318350985' id='player2' />
         </div>
       </div>
     )
